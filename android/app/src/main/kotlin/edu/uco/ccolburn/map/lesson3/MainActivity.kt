ca1e616f64ec6bc9f9310bf0d00a4fc1971dfdd8
Cody <ccolburn@uco.edu>
2021-01-23 10:00:53 -0600

    initial commit

---
+++ android/app/src/main/kotlin/edu/uco/ccolburn/map/lesson3/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.uco.ccolburn.map.lesson3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:lesson3/screen/signin_screen.dart';
+
+void main() {
+  runApp(PhotoMemoApp());
+}
+
+class PhotoMemoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: SignInScreen.routeName,
+      routes: {
+        SignInScreen.routeName: (context) => SignInScreen(),
+      },
+    );
+  }
+}
