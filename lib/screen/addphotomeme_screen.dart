8e36c051891d1b4d1ed0df3489c6765fc52465ff
Cody <ccolburn@uco.edu>
2021-01-24 14:41:05 -0600

    commit

--- lib/screen/addphotomeme_screen.dart
+++ lib/screen/addphotomeme_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:lesson3/model/constant.dart';
+import 'package:lesson3/model/photomemo.dart';
 
 class AddPhotoMemoScreen extends StatefulWidget {
   static const routeName = '/addPhotoMemoScreen';
@@ -11,6 +14,8 @@ class AddPhotoMemoScreen extends StatefulWidget {
 }
 
 class _AddPhotoMemoState extends State<AddPhotoMemoScreen> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  File photo;
   _Controller con;
   User user;
   @override
@@ -28,8 +33,93 @@ class _AddPhotoMemoState extends State<AddPhotoMemoScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add PhotoMemo'),
+        actions: [
+          IconButton(icon: Icon(Icons.check), onPressed: con.save),
+        ],
       ),
-      body: Text('add photomemo: ${user.email}'),
+      body: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.4,
+                    child: photo == null
+                        ? Icon(
+                            Icons.photo_library,
+                            size: 300,
+                          )
+                        : Image.file(
+                            photo,
+                            fit: BoxFit.fill,
+                          ),
+                  ),
+                  Positioned(
+                    right: 0.0,
+                    bottom: 0.0,
+                    child: Container(
+                      color: Colors.blue[200],
+                      child: PopupMenuButton<String>(
+                        onSelected: con.getPhoto,
+                        itemBuilder: (context) => <PopupMenuEntry<String>>[
+                          PopupMenuItem(
+                            value: Constant.SRC_CAMERA,
+                            child: Row(
+                              children: [
+                                Icon(Icons.photo_camera),
+                                Text(Constant.SRC_CAMERA),
+                              ],
+                            ),
+                          ),
+                          PopupMenuItem(
+                            value: Constant.SRC_GALLERY,
+                            child: Row(
+                              children: [
+                                Icon(Icons.photo_album),
+                                Text(Constant.SRC_GALLERY),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Title',
+                ),
+                autocorrect: true,
+                validator: PhotoMemo.validateMemo,
+                onSaved: con.saveTitle,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Memo',
+                ),
+                autocorrect: true,
+                keyboardType: TextInputType.multiline,
+                maxLines: 6,
+                validator: PhotoMemo.validateMemo,
+                onSaved: con.saveMemo,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'SharedWith (comma separated email list)',
+                ),
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                maxLines: 2,
+                validator: PhotoMemo.validateSharedWith,
+                onSaved: con.saveSharedWith,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
@@ -37,4 +127,31 @@ class _AddPhotoMemoState extends State<AddPhotoMemoScreen> {
 class _Controller {
   _AddPhotoMemoState state;
   _Controller(this.state);
+  PhotoMemo tempMemo = PhotoMemo();
+
+  void save() {
+    if (!state.formKey.currentState.validate()) return;
+    state.formKey.currentState.save();
+    print('==== ${tempMemo.title}');
+    print('==== ${tempMemo.memo}');
+    print('==== ${tempMemo.sharedWith}');
+  }
+
+  void getPhoto(String src) {
+    print('==== src: $src');
+  }
+
+  void saveTitle(String value) {
+    tempMemo.title = value;
+  }
+
+  void saveMemo(String value) {
+    tempMemo.memo = value;
+  }
+
+  void saveSharedWith(String value) {
+    if (value.trim().length != 0) {
+      tempMemo.sharedWith = value.split(RegExp('(,| )+')).map((e) => e.trim()).toList();
+    }
+  }
 }
